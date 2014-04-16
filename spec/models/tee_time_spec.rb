6633b4ff54b66c684a157c9329230b84a94a786e
Ziemek Wolski <ziemek.wolski@gmail.com>
2014-04-15 21:25:32 -0400

    Corrected some mislabeled tests

--- spec/models/tee_time_spec.rb
+++ spec/models/tee_time_spec.rb
@@ -23,61 +23,61 @@ describe TeeTime do
     let(:tee_time) {TeeTime.new}
 
     describe "invalid" do
-      it "should accept 1 minutes passed" do
+      it "should not accept 1 minutes passed" do
         tee_time.booking_time = Date.today.to_time + 1.minutes
         expect(tee_time.valid?).to eq(false)
         expect(tee_time.errors[:booking_time]).to include("must in intervals of 20")
       end
 
-      it "should accept 19 minutes passed" do
+      it "should not accept 19 minutes passed" do
         tee_time.booking_time = Date.today.to_time + 19.minutes
         expect(tee_time.valid?).to eq(false)
         expect(tee_time.errors[:booking_time]).to include("must in intervals of 20")
       end
 
-      it "should accept 21 minutes passed" do
+      it "should not accept 21 minutes passed" do
         tee_time.booking_time = Date.today.to_time + 21.minutes
         expect(tee_time.valid?).to eq(false)
         expect(tee_time.errors[:booking_time]).to include("must in intervals of 20")
       end
 
-      it "should accept 39 minutes passed" do
-        tee_time.booking_time = Date.today.to_time + 21.minutes
+      it "should not accept 39 minutes passed" do
+        tee_time.booking_time = Date.today.to_time + 39.minutes
         expect(tee_time.valid?).to eq(false)
         expect(tee_time.errors[:booking_time]).to include("must in intervals of 20")
       end
 
-      it "should accept 41 minutes passed" do
-        tee_time.booking_time = Date.today.to_time + 21.minutes
+      it "should not accept 41 minutes passed" do
+        tee_time.booking_time = Date.today.to_time + 41.minutes
         expect(tee_time.valid?).to eq(false)
         expect(tee_time.errors[:booking_time]).to include("must in intervals of 20")
       end
 
-      it "should accept 59 minutes passed" do
-        tee_time.booking_time = Date.today.to_time + 1.minutes
+      it "should not accept 59 minutes passed" do
+        tee_time.booking_time = Date.today.to_time + 59.minutes
         expect(tee_time.valid?).to eq(false)
         expect(tee_time.errors[:booking_time]).to include("must in intervals of 20")
       end
 
-      it "should accept 1 second passed" do
+      it "should not accept 1 second passed" do
         tee_time.booking_time = Date.today.to_time + 1.second
         expect(tee_time.valid?).to eq(false)
         expect(tee_time.errors[:booking_time]).to include("must in intervals of 20")
       end
 
-      it "should accept 20 second passed" do
+      it "should not accept 20 second passed" do
         tee_time.booking_time = Date.today.to_time + 20.second
         expect(tee_time.valid?).to eq(false)
         expect(tee_time.errors[:booking_time]).to include("must in intervals of 20")
       end
 
-      it "should accept 40 second passed" do
+      it "should not accept 40 second passed" do
         tee_time.booking_time = Date.today.to_time + 40.second
         expect(tee_time.valid?).to eq(false)
         expect(tee_time.errors[:booking_time]).to include("must in intervals of 20")
       end
 
-      it "should accept 59 second passed" do
+      it "should not accept 59 second passed" do
         tee_time.booking_time = Date.today.to_time + 59.second
         expect(tee_time.valid?).to eq(false)
         expect(tee_time.errors[:booking_time]).to include("must in intervals of 20")
